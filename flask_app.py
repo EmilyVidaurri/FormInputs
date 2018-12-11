dce5182b9e60afe36e8fa08e957b15558f172601
EmilyVidaurri <emily.vidaurri12@gmail.com>
2018-12-11 23:36:49 +0000

    added pseudo code

--- flask_app.py
+++ flask_app.py
@@ -14,3 +14,9 @@ def process_inputs():
     freeform = request.form.get('input_freeform', '')
     return render_template("main_page.html", input_data=dropdown,
                            output="You're a wizard %s." % name)
+#change the example select title favorite chicken nugget spot
+#favorie sauces in Example multiple select
+# change the example selects to chicken nugget spots
+# change example multiple selects to sauces
+# change example text area to tell us more about your love for chicken nuggets
+#change youre a Wizard to dif things based on answer chick-fil-a and
